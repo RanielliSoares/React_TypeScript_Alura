78e274b05d0084fa11c6c95e9d80142c25fcbffe
Ranielli <ranielli.pinho@knapp.com>
2025-01-21 16:07:11 -0300

    Criando Lista Tarefas

---
+++ alura-studies/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Formulario from './componentes/Formulario';
+import Lista from './componentes/Lista';
+
+function App() {
+  return (
+    <div className="App">
+     <Formulario/>
+     <Lista/>
+    </div>
+  );
+}
+
+export default App;
